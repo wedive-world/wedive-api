1fc4ac1fb2c7512072af1fcb64f6a34565ab5664
Mark Yun <only.hyeok@gmail.com>
2021-09-02 22:15:14 +0900

    fix

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 docker build . -t root/wedive-api &&\
 docker stop wedive-api || true &&
 docker rm wedive-api || true &&
-docker run -p 4001:4000 -d root/wedive-api --rm --name wedive-api
+docker run -p 4001:4000 -d  --name wedive-api --rm root/wedive-api
