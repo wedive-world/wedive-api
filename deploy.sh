560e98e0175d873cc96e9e0f5e0195bf3530c3e3
Mark Yun <only.hyeok@gmail.com>
2021-10-07 23:35:25 +0900

    fixed deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
-cp /wedive-secret/db-config.env ./wedive-secret/ &&
-cp /wedive-secret/s3-config.env ./wedive-secret/ &&
-cp /wedive-secret/aws-secret.env ./wedive-secret/ &&
+cp /wedive-secret/db-config.env ./wedive-secret/ || true &&
+cp /wedive-secret/s3-config.env ./wedive-secret/ || true &&
+cp /wedive-secret/aws-secret.env ./wedive-secret/ || true &&
 docker-compose build &&
 docker-compose up -d &&
 docker image prune -a -f
