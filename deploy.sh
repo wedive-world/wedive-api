1ac407b0c25af5d9cfa3918fc3641badf4b2fcf0
Mark Yun <only.hyeok@gmail.com>
2021-10-07 23:22:18 +0900

    added submodule

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
-cp /wedive-secret/api-config.env ./ &&
-cp /wedive-secret/api-secret.env ./ &&
+cp /wedive-secret/api-config.env ./wedive-secret/ &&
+cp /wedive-secret/api-secret.env ./wedive-secret/ &&
 docker-compose build &&
 docker-compose up -d &&
 docker image prune -a -f
--- deploy_dev.sh
+++
@@ -1,5 +0,0 @@
-cp ../wedive-secret/api-config.env ./ &&
-cp ../wedive-secret/api-secret.env ./ &&
-docker-compose build &&
-docker-compose up -d &&
-docker image prune -a -f
