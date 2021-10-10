4290b8a4a0ea70cc99dee7af047e993ef4dffe2f
Mark Yun <only.hyeok@gmail.com>
2021-10-10 09:58:24 +0900

    applied ssh deploy

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,3 @@
-cp /wedive-secret/db-config.env ./wedive-secret/ || true &&
-cp /wedive-secret/s3-config.env ./wedive-secret/ || true &&
-cp /wedive-secret/aws-secret.env ./wedive-secret/ || true &&
 docker-compose build &&
 docker-compose up -d &&
 docker image prune -a -f
