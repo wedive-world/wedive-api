90be1c38b49189553b2b9445d9b2d90107e5f50f
jjangs <jjangs85@gmail.com>
2021-10-24 14:20:50 +0900

    temp

--- graphql/resolvers/util/translator.js
+++ graphql/resolvers/util/translator.js
@@ -19,8 +19,8 @@ module.exports.translateIn = (originalObject, inputObject, countryCode) => {
 module.exports.translateOut = (object, countryCode) => {
     properties
         .filter(property => object.hasOwnProperty(property))
-        .forEach(property => {
-            object[property] = object[property + 'Translation'][countryCode]
-        });
+        //.forEach(property => {
+        //    object[property] = object[property + 'Translation'][countryCode]
+        //});
     return object
 }
