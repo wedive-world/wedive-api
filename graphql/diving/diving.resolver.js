b4806d5559e594443e44070c42179c98b09d0740
Mark Yun <only.hyeok@gmail.com>
2022-01-31 15:54:35 +0900

    fixed error

--- graphql/diving/diving.resolver.js
+++ graphql/diving/diving.resolver.js
@@ -96,7 +96,7 @@ module.exports = {
             }
 
             let userUid = context.uid
-            if (diving.hostUser.uid != userUid) {
+            if (diving.hostUser.uid == userUid) {
                 return {
                     success: false,
                     reason: 'hostCannotApply'
