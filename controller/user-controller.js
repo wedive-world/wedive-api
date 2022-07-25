f3768fec034c8d679940de79085ab4feffbae7de
Mark Yun <only.hyeok@gmail.com>
2022-07-26 00:11:47 +0900

    added uid guard in userController

--- controller/user-controller.js
+++ controller/user-controller.js
@@ -5,6 +5,10 @@ const {
 module.exports.getUserIdByUid = getUserIdByUid
 
 async function getUserIdByUid(uid) {
+    if (!uid) {
+        throw Error('uid is null')
+    }
+    
     let user = await User.findOne({ uid: uid })
         .select('_id')
         .lean()
